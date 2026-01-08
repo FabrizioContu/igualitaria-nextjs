057fd51512b21f99c929cef6f652657df83e3666
Fabrizio Contu <fab.contu@gmail>
2026-01-08 12:15:02 +0100

    feat: add base UI components

---
+++ components/ui/Spinner.tsx
@@ -0,0 +1,11 @@
+export const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div
+        className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"
+        aria-hidden="true"
+        sr-only
+      ></div>
+    </div>
+  );
+};
